e7bb2daee53a789768a384eecae0024a3a83744b
Jan Hendrickx <admin@janhendrickx.be>
2021-10-05 09:06:43 +0200

    all sorts of small changes

--- site/web/build/js/scripts.js
+++ site/web/build/js/scripts.js
@@ -3,4 +3,19 @@ jQuery(document).ready(function(){
     $('#close').click(function() {
         $('.c-blog-detail').animate({"margin-left": "-1000px"});
     })
+
+    // $('.c-bridges-list-item').click(function() {
+    //     $('.accordion-icon').toggleClass('open');
+    //     // $('.accordion-icon').toggleClass('open');
+
+    //     // $accordion_content = $(this).next('.accordion-content');
+    //     // $('.accordion-content').not($accordion_content).slideUp();
+    //     // $('.accordion-content').not($accordion_content).prev('.accordion-title').removeClass('open');
+    //     // $accordion_content.stop(true, true).slideToggle(400)
+
+    //     // Remove this handle
+    //     //$('.accordion-title i').toggleClass('rotate');
+    // });
+
+  
 });
